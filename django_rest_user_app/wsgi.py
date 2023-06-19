fb274367a499b9cd876e261dba882041e7ee809c
mpeterit <maxim.peter.it@gmail.com>
2023-06-19 23:18:20 +0200

    Renamed directory

--- django_rest_user_app/wsgi.py
+++ django_rest_user_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_user_app_project project.
+WSGI config for django_rest_user_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_user_app_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_rest_user_app.settings')
 
 application = get_wsgi_application()
